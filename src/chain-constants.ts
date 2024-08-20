4f5506c2981a8d0aae06ea1b53f4ba0a8a84f651
kiwi0401 <kiwi04011@gmail.com>
2024-08-20 12:25:23 -0700

    add linea

--- src/chain-constants.ts
+++ src/chain-constants.ts
@@ -10,6 +10,7 @@ import {SymbioticVault} from "./constants";
 const _XUltraLRTAddressSEPOLIA = "0x633dc76965e520a777378CFc6299d925B443C224";
 const _XUltraLRTAddressBSC = "0x7e80886220B586942a200c92AD1273A3e128086b"
 const _XUltraLRTAddressBLAST = "0xbb4e01B8940E8E2b3a95cED7941969D033786FF7"
+const _XUltraLRTAddressLINEA = "0xB838Eb4F224c2454F2529213721500faf732bf4d"
 const _XUltraLRTAddressETH = "0x91F822fAFc1db552e78f49941776aCB2a78fD422"
 
 const ultraLRTTokenBlast = "0xbb4e01B8940E8E2b3a95cED7941969D033786FF7"
@@ -130,5 +131,17 @@ export const NETWORK_PARAMS: { [index: number]: NetworkParams } = {
         blockExplorerUrls: ["https://blastscan.io/"],
         xUltraLRTAddress: _XUltraLRTAddressBLAST,
         ultraLRTAddress: ultraLRTTokenBlast
+    },
+    59144: {
+        chainName: "Linea",
+        nativeCurrency: {
+            name: "Ether",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: ["https://linea-mainnet.g.alchemy.com/v2/T8JHI8jMGKrO88j6qvZwjk8d1IxOPqtk"],
+        blockExplorerUrls: ["https://lineascan.build/"],
+        xUltraLRTAddress: _XUltraLRTAddressLINEA,
+        ultraLRTAddress: _XUltraLRTAddressLINEA
     }
 };
